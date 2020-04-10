6af7d533694b371d2451399e093845c4c950093c
davidsmiles <ugberodavid@gmail.com>
2020-04-10 03:54:45 +0100

    add uwsgi ini file to use production server

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.secret_key = 'iamdoingwell'
 
 
 @app.route('/')
@@ -9,4 +10,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
