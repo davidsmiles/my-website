8951c636a489e895aa30f04ed9de50b77b75e7c2
davidsmiles <ugberodavid@gmail.com>
2020-04-12 05:11:47 +0100

    configured website for docker container

--- flask/default_config.py
+++ flask/default_config.py
@@ -1,7 +1,7 @@
 import os
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = os.environ.get("PROD_DATABASE_URL", "sqlite:///data/db")
+SQLALCHEMY_DATABASE_URI = os.environ.get("PROD_DATABASE_URL", "sqlite:///data.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PROPAGATE_EXCEPTIONS = True
 APP_SECRET_KEY = 'iamdoingwell'
